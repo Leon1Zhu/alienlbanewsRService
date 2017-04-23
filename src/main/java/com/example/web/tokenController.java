7c2f501dcf57439a110a71c349d367488c66a0a8
zhuliang <937154020@qq.com>
2017-04-23 15:46:20 +0800

    Initial commit

---
+++ src/main/java/com/example/web/tokenController.java
@@ -0,0 +1,33 @@
+package com.example.web;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhuliang on 2017/4/5.
+ */
+@RestController
+public class tokenController {
+    @Value("${jwt.TTLMillis}")
+    private Long TTLMillis;
+    @Value("${jwt.audience}")
+    private String audience;
+    @Value("${jwt.base64Security}")
+    private String base64Security;
+
+    @RequestMapping(value="/getToken",method = RequestMethod.POST)
+    public ResponseEntity test(){
+       String token = JwtUtils.createJWT("zhuliang","123456","",audience,"test",TTLMillis,base64Security);
+        JSONObject jo = new JSONObject();
+        jo.put("token",token);
+       return  ResponseEntity.ok().body(jo);
+    }
+}

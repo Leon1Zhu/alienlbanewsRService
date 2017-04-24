debce1087016f4d9216302d27eff1fe82b6080c3
zhuliang <937154020@qq.com>
2017-04-24 13:03:50 +0800

    配置文件修改以及打包模式的修改

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -9,4 +9,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	/*	@Bean
+	public TomcatEmbeddedServletContainerFactory servletContainer(){
+		TomcatEmbeddedServletContainerFactory container = new TomcatEmbeddedServletContainerFactory();
+		container.setPort(8081);
+//      container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,""));
+		return container;
+	}*/
 }
